f09d314db728dc085a3c5ba26573bbe2d02185a2
Federico <federicomasucci17@gmail.com>
2026-07-04 19:03:50 +0200

    modified:   esercizio-1/src/components/Counter.jsx

--- esercizio-1/src/components/Counter.jsx
+++ esercizio-1/src/components/Counter.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react';
 
 function Counter() {
-    const [count, setCount] = useState(0);
+    let [count, setCount] = useState(0);
 
     function incrementa(valore) {
         setCount(count + 1);
     }
+
+    function Azzera(valore) {
+        setCount(count = 0);
+    }
     return (
-        <button type="button" className="btn btn-primary" onClick={() => incrementa(1)}>
-            Premi il pulsante per incrementare {count}</button>
+        <>
+            <button type="button" className="btn btn-primary" onClick={() => incrementa(1)}>
+                Premi il pulsante per incrementare {count}</button>
+
+
+            <button type="button" className="btn btn-danger" onClick={() => Azzera()}>Azzera</button>
+
+        </>
     );
 
 }
